e8337f1c0d54a52d385dc3b2ada271da21816a8c
Fouza <gf_oukacha@esi.dz>
2024-11-12 21:19:24 +0100

    new

--- api/orders.js
+++ api/orders.js
@@ -72,7 +72,46 @@ export default () => {
         }
 
     })
+    router.patch('/:orderId/products', async (req, res) => {
+        const { orderId } = req.params;
+        const { products } = req.body;
 
+
+        try {
+
+            const order = await ordersCollection.findById(orderId);
+            if (!order) {
+                return res.status(404).send('Order not found');
+            }
+
+            if (['shipped', 'delivered'].includes(order.status)) {
+                return res.status(400).send('Cannot modify an order that has been shipped or delivered');
+            }
+
+            for (let item of products) {
+                const product = await productsCollection.findById(item.product_id);
+                if (!product) {
+                    return res.status(400).send(`Product with ID ${item.product_id} not found`);
+                }
+                if (product.stock < item.quantity) {
+                    return res.status(400).send(`Insufficient stock for product ${product.name}`);
+                }
+
+                const newUpdatePro = await productsCollection.findOneAndUpdate({ _id: item.product_id }, { stock: product.stock - item.quantity }, { returnDocument: 'after' })
+            }
+
+            const newUpdate = await ordersCollection.findOneAndUpdate({ _id: orderId }, { products: [...order.products, ...products] }, { returnDocument: 'after' })
+
+            if (newUpdate) {
+                res.send({ success: true, user: newUpdate })
+            } else {
+                res.send({ success: false, message: 'Server Error' })
+            }
+
+        } catch {
+            res.status(500).send('Error updating order');
+        }
+    });
     
     return router
 }
